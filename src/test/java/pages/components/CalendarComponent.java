7211c57787a0ec1d39c36b054fe80d8c428e049a
Кирилл Гольцер <kirill.holzer@yandex.ru>
2024-10-10 20:53:57 +0300

    ADDED DataGenerator + Refactor (ready for review)

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,6 +6,6 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__year-select").$(byText(year)).click();
         $(".react-datepicker__month-select").$(byText(month)).click();
-        $(".react-datepicker__day--00" + day).click();
+        $(".react-datepicker__day--0" + day).click();
     }
 }
