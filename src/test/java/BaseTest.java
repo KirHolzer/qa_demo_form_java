d6a2ecf723d5aed96a3d3b6cdf4ee92f80e0737e
Кирилл Гольцер <kirill.holzer@yandex.ru>
2024-10-20 20:12:50 +0300

    ADDED afterEach Attach

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -10,6 +12,14 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         // Configuration.holdBrowserOpen = true;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
